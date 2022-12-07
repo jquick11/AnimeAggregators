59102c661576b3c6df6d1490d590964c02173e58
Elaine Wu <elainewu@Elaines-MacBook-Air-6.local>
2022-12-07 11:44:11 -0500

    new file

--- animeList.py
+++ animeList.py
@@ -7,25 +7,8 @@ response = requests.get(url)
 print(response.text)
 
 
-
-# def write_json(cache_filename, anime):
-#     json_object = json.dumps(anime, indent=1)
-#     with open(cache_filename, 'w') as f:
-#         f.write(json_object)
-
-
 # 1: got the url request information
-# 2: process the data and got the title, with rank and score in tuple
-# url= 'https://api.jikan.moe/v4/top/anime?page={}'
-# anime_info = {}
-# for i in range(5):
-#     u = url.format(i)
-#     response = requests.get(u)
-#     val = response.json()
-#     search_result = val['data']
-#     for anime_data in search_result:
-#         anime_info[anime_data['title']] = (anime_data['rank'],anime_data['score'])
-# print(anime_info)
+
 
 # process the data and make sure each rank show up, its possible because there is same score ratings
 url= 'https://api.jikan.moe/v4/top/anime?page={}'
